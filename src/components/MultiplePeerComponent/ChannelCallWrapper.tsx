7233d57419b60c60e62b16319e2aefef4e011532
Ioannes <ivan.efimov2281@gmail.com>
2023-06-02 13:49:07 +0300

    adding channel call wrapper

---
+++ src/components/MultiplePeerComponent/ChannelCallWrapper.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useAuthStore } from '../../ZustandStore'
+import { useResponseContext, useCurrentChannel } from '../../hooks'
+import Hamburger from '../HamburgerMenu/Hamburger'
+import MessagesProvider from '../MessagesWrapper/Context/MessagesContext'
+import MessagesWrapper from '../MessagesWrapper/MessagesWrapper'
+import MultiplePeerComponent from './MultiplePeerComponent'
+
+function ChannelCallWrapper() {
+    const setLoading = useAuthStore(state=>state.setLoading)
+    const user = useAuthStore(state=>state.user)
+    const {setServerResponse} = useResponseContext()
+    const {channel_id}=useParams()
+    const {currentChannel,setCurrentChannel,addCurrentChannelMessage,currentChannelMessages,deleteCurrentChannelMessage,isLoading}=useCurrentChannel(channel_id ?? '',user)
+  
+    const content = (
+        <div className="video-chat-wrapper">
+            <Hamburger type='messages'>
+                <MessagesProvider>
+                    <MessagesWrapper setCurrentChannel={setCurrentChannel} currentChannel={currentChannel} currentChannelMessages={currentChannel?.messages!}/>
+                </MessagesProvider>
+            </Hamburger>
+
+            <MultiplePeerComponent currentChannel={currentChannel !}/>
+        </div>
+    )
+  return content
+}
+
+export default ChannelCallWrapper
